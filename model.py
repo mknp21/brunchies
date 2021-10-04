ada7219966559115a419769854652a6feb2d2263
Mika Patterson <mknpatt21@gmail.com>
2021-10-04 22:41:11 +0000

    Created connect to db function

--- model.py
+++ model.py
@@ -33,7 +33,16 @@ class Restaurants(db.Model):
 
     def __repr__(self):
         return f"Restaurant name={self.rest_name} category={self.category}"
-    
+
+def connect_to_db(flask_app, db_uri="postgresql:///brunch", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
 
 if __name__ == "__main__":
     from server import app
