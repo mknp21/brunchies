c03a0e2529fac7e882a7d45b186f708a13f99514
Mika Patterson <mknpatt21@gmail.com>
2021-10-04 21:48:53 +0000

    Created a user class in model.py

--- model.py
+++ model.py
@@ -4,7 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """A user."""
 
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Columm(db.String(50), nullable=False)
+    email = db.Column(db.String(25), nullable=False)
+    pw = db.Column(db.String(20), nullable=False)
+    zipcode = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f"User name={self.name} email={self.email}"
 
 if __name__ == "__main__":
     from server import app
