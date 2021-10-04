a711615ed402a21ae1e9a9badcc98fc4b2ec00f7
Mika Patterson <mknpatt21@gmail.com>
2021-10-04 19:46:45 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+"""Server for brunch locator app."""
+
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db
+# import crud
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def show_homepage():
+    """Show the homepage"""
+
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
