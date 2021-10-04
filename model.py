e3951915f21ce706f909fea6013c036d6556af03
Mika Patterson <mknpatt21@gmail.com>
2021-10-04 21:32:18 +0000

    Initial commit for model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+"""Models for brunch locator app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
