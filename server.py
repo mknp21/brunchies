552b010b435b55ae1774eff73b9fcf77c9630e6b
Mika Patterson <mknpatt21@gmail.com>
2021-10-04 21:29:08 +0000

    Added login route in server.py, fixed homepage, and completed basic login page layout

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for brunch locator app."""
 
 from flask import (Flask, render_template, request, flash, session, redirect)
-from model import connect_to_db
+# from model import connect_to_db
 # import crud
 from jinja2 import StrictUndefined
 
@@ -11,11 +11,17 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def show_homepage():
-    """Show the homepage"""
+    """Show the homepage."""
 
     return render_template("homepage.html")
 
+@app.route('/login')
+def show_login_page():
+    """Show login page."""
+
+    return render_template("login_page.html")
+
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
-    connect_to_db(app)
+    # connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
