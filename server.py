a126842ac946cd03de5163d7904c1db4dd86f5fb
Mika Patterson <mknpatt21@gmail.com>
2021-10-05 22:17:03 +0000

    Created routes and started functions for brunch search form and search results

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 """Server for brunch locator app."""
 
 from flask import (Flask, render_template, request, flash, session, redirect)
-# from model import connect_to_db
+from model import connect_to_db
 # import crud
 from jinja2 import StrictUndefined
+
+from pprint import pformat
+import os
+import requests
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+API_KEY = os.environ['YELP_KEY']
+
 
 @app.route('/')
 def show_homepage():
@@ -15,11 +22,26 @@ def show_homepage():
 
     return render_template("homepage.html")
 
+
 @app.route('/login')
 def show_login_page():
     """Show login page."""
 
     return render_template("login_page.html")
+
+
+@app.route('/brunchspots')
+def show_brunch_form():
+    """Show brunch search form."""
+    
+    pass
+
+
+@app.route('/brunchspots/search')
+def find_brunch_spots():
+    """Search for brunch spots on Yelp."""
+
+    pass
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
