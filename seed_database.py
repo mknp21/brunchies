bc527f3af03a5db1429f6ad1812e9ae707e4da0b
Mika Patterson <mknpatt21@gmail.com>
2021-10-06 21:57:00 +0000

    Seeded the db with user info

--- seed_database.py
+++ seed_database.py
@@ -8,15 +8,33 @@ import crud
 import model
 import server
 
-os.system('dropdb ratings')
-os.system('createdb ratings')
+API_KEY = os.environ['YELP_KEY']
+headers = {'Authorization': 'Bearer %s' % API_KEY}
+
+os.system('dropdb brunch')
+os.system('createdb brunch')
 
 model.connect_to_db(server.app)
 model.db.create_all()
 
 # Create a user
-user = crud.create_user(email, password)
+for n in range(5):
+    name = 'First Last'
+    email = f'testuser{n}@test.com'
+    pw = f'password{n}'
+    zipcode = f'5555{n}'
+
+    user = crud.create_user(name, email, pw, zipcode)
 
 # Create a restaurant
+url = 'https://api.yelp.com/v3/businesses/search'
+params = {'location':'Bay Area', 'categories':'breakfast_brunch'}
+
+res = requests.get(url, params=params, headers=headers)
+# print(f'The status code is {res.status_code}')
+
+# return json.loads(res.text)
+print(json.loads(res.text))
+# res.business[id]
 
 # Create a save item
