46670277b80d80c94c344163bfd7aa59170f1768
Mika Patterson <mknpatt21@gmail.com>
2021-10-12 23:05:17 +0000

    Started save restaurant fxn

--- static/js/brunch-app.js
+++ static/js/brunch-app.js
@@ -2,3 +2,10 @@
 
 // Save a restaurant and add it to the user's list of saved restaurants
 
+// const saveRestaurant = () => {
+//   $('.saved-restaurants').append
+// }
+
+$('#save-restaurant').on('click', () => {
+    alert("Button Clicked!");
+})
