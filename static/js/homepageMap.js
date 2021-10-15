8301d69ba6a26b39d33206789472c27dea30c6d8
Mika Patterson <mknpatt21@gmail.com>
2021-10-15 22:02:45 +0000

    Added scripts to html file to acces Google Maps API and created a fxn to initialize the map

--- static/js/homepageMap.js
+++ static/js/homepageMap.js
@@ -1,2 +1,17 @@
-"use strict";
+'use strict';
+
+const map;
+
+function initMap() {
+    const sfCoords = {
+        lat: 37.601773,
+        lng: -122.20287,
+    };
+    
+    map = new google.maps.Map(document.querySelector('#map'), {
+        center: sfCoords,
+        zoom: 10,
+    });
+
+}
 
