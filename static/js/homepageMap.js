80afd390ac947fc2025e67891b9423ab17ba0661
Mika Patterson <mknpatt21@gmail.com>
2021-10-20 23:03:16 +0000

    Changed styling of all maps

--- static/js/homepageMap.js
+++ static/js/homepageMap.js
@@ -7,17 +7,115 @@ function initMap() {
         lat: 37.7749,
         lng: -122.4194,
     };
-    
-    map = new google.maps.Map(document.querySelector('#map'), {
-        center: sfCoords,
+
+    const mapOptions = {
+        scrollwheel: false,
         zoom: 12,
-    });
+        center: sfCoords,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "lightness": -5
+                    },
+                    {
+                        "color": "#b0b0b0"
+                    },
+                    {
+                        "weight": 1.7
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    },
+                    {
+                        "lightness": 26
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    },
+                    {
+                        "lightness": 66
+                    }
+                ]
+            }
+        ]
+    };
+
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
     const brunchMarker = new google.maps.Marker({
         position: sfCoords,
         title: 'SF',
         map: map,
-        icon: "/static/img/pink-pushpin.png"
+        icon: '/static/img/pink-pushpin.png'
     });
 
 }
--- static/js/savedPlacesMap.js
+++ static/js/savedPlacesMap.js
@@ -11,15 +11,105 @@ function initMap() {
     map = new google.maps.Map(document.querySelector('#map'), {
         center: sfCoords,
         zoom: 12,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "lightness": -5
+                    },
+                    {
+                        "color": "#b0b0b0"
+                    },
+                    {
+                        "weight": 1.7
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    },
+                    {
+                        "lightness": 26
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ddc0b4"
+                    },
+                    {
+                        "lightness": 66
+                    }
+                ]
+            }
+        ]
     });
 
     $.get('/save-data.json', res => {
         for (const restaurant of res.results) {
-    
-            // const placeCoords = {
-            //     lat: Number(restaurant.lat),
-            //     long: Number(restaurant.long)
-            // };
 
             const placeCoords = new google.maps.LatLng(Number(restaurant.lat), Number(restaurant.long));
     
@@ -33,23 +123,3 @@ function initMap() {
     });
 }
 
-
-
-// let savedItem = $('#saved-item');
-
-// savedItem.on('mouseover', () => {
-//     alert('Works!');
-// });
-
-// $('#saved-item').on('mouseover', () => {
-//     $.post('/saved', res => {
-//         alert(res)
-//     })
-// })
-
-// $.post('/saved', response => {
-//     const savedCoord = {
-//         latitude:
-//         longitude:
-//     }
-// })
