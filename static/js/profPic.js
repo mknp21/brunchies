d61ef5d5a9db3f120b130e6bc2ea342475b178f7
Mika Patterson <mknpatt21@gmail.com>
2021-10-27 06:37:40 +0000

    Added ability to upload a profile pic

--- static/js/profPic.js
+++ static/js/profPic.js
@@ -1,9 +1,29 @@
-const imgDiv = $('.profile-pic-div');
-const img = $('#photo');
-const file = $('#file');
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
 const uploadBtn = document.querySelector('#uploadBtn')
 
-// if user hovers on profile div
-imgDiv.on('mouseover', function() {
+// if user hovers on prof div
+imgDiv.addEventListener('mouseover', function() {
     uploadBtn.style.display = 'block';
+});
+
+// if user hovers away from prof div
+imgDiv.addEventListener('mouseleave', function() {
+    uploadBtn.style.display = 'none';
+});
+
+file.addEventListener('change', function() {
+    // this. notation refers to the file variable
+    const chosenFile = this.files[0];
+
+    if (chosenFile) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function() {
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(chosenFile);
+    }
 });
